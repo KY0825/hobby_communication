02a7cea33c5ff4f2b6990bff38dae8d93622ed7b
KY0825 <mouko.ky@gmail.com>
2020-11-02 14:30:23 +0900

    chatマイグレーションの修正

--- db/migrate/20201030090007_create_chats.rb
+++ db/migrate/20201030090007_create_chats.rb
@@ -1,11 +1,10 @@
 class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
-      t.string     :nickname,   null: false
-      t.text       :text,       null: false
-      t.references :hobic,      foreign_key: true
-      t.references :user,       foreign_key: true
-      t.references :room,       foreign_key: true
+      t.text         :text,       null: false
+      t.references   :hobic,      foreign_key: true
+      t.references   :user,       foreign_key: true
+      t.references   :room,       foreign_key: true
       t.timestamps
     end
   end
