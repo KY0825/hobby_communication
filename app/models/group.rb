0bb339cf1ea362510af2d415fe553d173e74a470
KY0825 <mouko.ky@gmail.com>
2020-11-02 14:31:18 +0900

    groupモデルの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,24 @@
 class Group < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :genre_id
+    validates :user
+  end
+
+  has_one_attached :image
+  belongs_to :hobic
+  belongs_to :user
+  has_many :messages
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :genre
+
+  with_options numericality:{other_than: 1} do
+    validates :category_id
+    validates :genre_id
+  end
+
 end
