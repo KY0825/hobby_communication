4fd2018d870355db74be6fd8b5a3aa434de48e83
KY0825 <mouko.ky@gmail.com>
2020-11-02 14:30:40 +0900

    messageモデルの修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,11 @@
 class Message < ApplicationRecord
+  with_options presence: true do
+    validates :text
+  end
+
+  has_one_attached :image
+  belongs_to :hobic
+  belongs_to :user
+  belongs_to :group
+
 end
