c20e0525af73d8f3e9417d977f5aaedf1e90f288
KY0825 <mouko.ky@gmail.com>
2020-11-02 14:02:16 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'hobics#index'
   devise_for :users
   resources :hobics, only: [:index]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :chats, only: [:index, :create]
+  end
+  resources :groups, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
